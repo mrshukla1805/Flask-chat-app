39828ecbefcf20ab9013c9569456b1eb20891337
mrshukla <54001817+mrshukla1805@users.noreply.github.com>
2020-02-07 13:52:55 +0530

    Update __init__.py

--- flasksocket/__init__.py
+++ flasksocket/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.secret_key = "secret"
 
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://qtdkiadqujlwkt:32245eeb235f074e76f4f3e597b7686414ba09c9a8a1a4bbff95df560dc34544@ec2-174-129-32-240.compute-1.amazonaws.com:5432/d5f7lfpq6tn46o'
+app.config['SQLALCHEMY_DATABASE_URI']= ''
 db = SQLAlchemy(app)
 
 login_m = LoginManager(app)
